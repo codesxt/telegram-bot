00065efd83555050a7da1b2fa48a5a9fa7cc204d
Bruno Faúndez Valenzuela <gameover_sxt_nah@hotmail.com>
2016-04-27 18:02:45 -0300

    First commit.

---
+++ cam.py
@@ -0,0 +1,8 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+ret, frame = cap.read()
+gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+cv2.imwrite('images/default.jpg',frame)
+cap.release()
---
+++ telepot-test.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import telepot
+import time
+import datetime
+import numpy as np
+import cv2
+
+def handle(msg):
+	chat_id = msg['from']['id']
+	command = msg['text']
+
+	print 'Mensaje recibido: %s' % command
+	if command == '/hora':
+		bot.sendMessage(chat_id, 'La hora es: ' + str(datetime.datetime.now()))
+	elif command == '/help':
+		bot.sendMessage(chat_id, 'Ayura')
+	elif command == '/photo':
+		bot.sendMessage(chat_id, 'Solicitando imágen de la cámara...')
+		bot.sendChatAction(chat_id, 'upload_photo')
+		cap = cv2.VideoCapture(0)
+		ret, frame = cap.read()
+		cv2.imwrite('images/default.jpg',frame)
+		cap.release()
+		result = bot.sendPhoto(chat_id, open('images/default.jpg', 'rb'), caption='Imagen desde la cámara.')
+	else:
+		bot.sendMessage(chat_id, 'Lo siento. No entendí tu mensaje.\nPuedes escribir el comando \'/help\' para obtener ayuda.')
+
+bot = telepot.Bot('211389236:AAFvvA0oyQD31kfmmDvTWQN9F26T-Xd12ls')
+bot.notifyOnMessage(handle)
+print 'Estoy escuchando...'
+
+while 1:
+	time.sleep(10)
